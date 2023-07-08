de6aa87098649b4632d7bca9caa5d0cb8bb0567f
yehonatanyosefi <123459744+yehonatanyosefi@users.noreply.github.com>
2023-07-08 20:52:08 +0300

    added chat interface barebones

--- webviews/components/ChatWindow.svelte
+++ webviews/components/ChatWindow.svelte
@@ -1,2 +1,22 @@
-<script lang='ts'></script>
-<h1>Hello Code</h1>
+<script lang='ts'>
+    let message: string = '';
+    let messages: string[] = [];
+
+    const sendMessage = () => {
+        if (message.trim() !== '') {
+            messages = [...messages, message];
+        }
+        message = '';
+    };
+</script>
+
+
+
+<h1>Chat Assistant</h1>
+<div id="chatBox">
+    {#each messages as msg (msg)}
+        <p>{msg}</p>
+    {/each}
+</div>
+<input type="text" bind:value={message} on:keydown={(e) => { if(e.key === 'Enter') sendMessage(); }} />
+<button on:click={sendMessage}>Send</button>
